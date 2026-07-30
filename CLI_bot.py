fb2b87b35fae5028dfa59d9f75b95199f9ef405b
ameratto <q27www@gmail.com>
2026-07-30 21:51:06 +0300

    Oleksii

--- CLI_bot.py
+++ CLI_bot.py
@@ -1,92 +1,75 @@
-from dataclasses import dataclass
-from collections import UserDict
-import re
-from os.path import join
+def error_handler(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please"
+        except IndexError:
+            return "Give me correct name"
+        except KeyError:
+            return "Give me correct name"
+
+    return inner
 
 
-@dataclass
-class Furniture:
-    material: str
-    weight: float
-    height: float
-    width: float
-    length: float
-    _cost: float
-
-    def calculate_discount(self, customer):
-        return self._cost - customer.get_discount()/100 * self._cost
-
-class Customer(UserDict):
-    def get_discount(self):
-        return float(re.search(r'\d', self.data["discount"]).group())
-
-    def get_first_name(self):
-        return self.data['name'].split()[0]
-
-    def get_last_name(self):
-        return self.data['name'].split()[1]
-
-    def get_email(self):
-        return self.data['email']
+@error_handler
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
 
 
-# class Particle:
-#     def __init__(self):
-#         self._weight = 0.0
-#         self._velocity = 0.0
-#         self._acceleration = 0.0
-#
-#     def get_weight(self):
-#         return self._weight
-#
-#     def get_velocity(self):
-#         return self._velocity
-#
-#     def get_acceleration(self):
-#         return self._acceleration
-#
-#     #@reduction_to_ftype
-#     def set_weight(self, weight):
-#         self._weight = weight
-#
-#     #@reduction_to_ftype
-#     def set_velocity(self, velocity):
-#         self._velocity = velocity
-#
-#     #@reduction_to_ftype
-#     def set_acceleration(self, acceleration):
-#         self._acceleration = acceleration
+@error_handler
+def change_contact(args, contacts):
+    name, new_phone = args
+    if contacts[name]:
+        contacts[name] = new_phone
+        return "Contact changed."
+    else:
+        return "Contact not found."
+
+@error_handler
+def show_phone(args, contacts):
+    name = args[0]
+    return contacts[name]
+
+
+def show_all(contacts):
+    return [f"{name}: {phone}" for name, phone in contacts.items()]
+
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
 
 
 def main():
-    contacts = [
-        {
-            "name": "Allen Raymond",
-            "email": "nulla.ante@vestibul.co.uk",
-            "age": "26",
-            "phone": "(992) 914-3792",
-            "discount": "10%"
-        },
-        {
-            "name": "Chaim Lewis",
-            "email": "dui.in@egetlacus.ca",
-            "age": "18",
-            "phone": "(294) 840-6685",
-            "discount": "10%"
-        },
-        {
-            "name": "Kennedy Lane",
-            "email": "mattis.Cras@nonenimMauris.net",
-            "age": "101",
-            "phone": "(542) 451-7038",
-            "discount": "10%"
-        }
-    ]
+    contacts = {}
 
-    users = [Customer(el) for el in contacts]
-    chair = Furniture("Wood", 20.2, 205.0, 41.1, 60.0, 2000.99)
-
-    print(f"Cost of {chair} for {users[0]['name']}: {chair.calculate_discount(users[0])}")
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Please enter a command: ").strip().lower()
+        if not user_input:
+            print("Invalid command. Try again.")
+            continue
+        command, *args = parse_input(user_input)
+        match command:
+            case "hello":
+                print("How can I help you?")
+            case "add":
+                print(add_contact(args, contacts))
+            case "change":
+                print(change_contact(args, contacts))
+            case "phone":
+                print(show_phone(args, contacts))
+            case "all":
+                print(show_all(contacts))
+            case "exit" | "close":
+                print("Goodbye!")
+                break
+            case _:
+                print("Invalid command. Try again.")
 
 
 if __name__ == '__main__':
